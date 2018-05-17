97c91f2539a90d798adb205ef3d06324507f055e
Srishan Bhattarai <srishanbhattarai@gmail.com>
2018-05-17 05:58:18 +0545

    (fix) Use keyed fields

--- main.go
+++ main.go
@@ -15,6 +15,13 @@ func main() {
 }
 
 func showDate(yy, mm, dd int) {
-	bs := conversion.ToBS(conversion.Epoch{yy, int(mm), dd})
+	bs := conversion.ToBS(
+		conversion.Epoch{
+			Year:  yy,
+			Month: int(mm),
+			Day:   dd,
+		},
+	)
+
 	fmt.Printf("%s %d, %d\n", conversion.BSMonths[bs.Month], bs.Day, bs.Year)
 }
