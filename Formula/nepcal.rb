235d4862238631c0f8702c4fb3681b4a6b9798bd
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-14 22:17:21 +0545

    Brew formula update for nepcal version v0.4.0

--- Formula/nepcal.rb
+++ Formula/nepcal.rb
@@ -3,7 +3,7 @@ class Nepcal < Formula
   homepage "https://github.com/nepcal/nepcal"
   url "https://github.com/srishanbhattarai/nepcal/releases/download/v0.4.0/nepcal_0.4.0_darwin_amd64.tar.gz"
   version "0.4.0"
-  sha256 "39bc4a91c44623a947c0fc32d1292ab8f17fd1637b1db107abb1f00976716d9d"
+  sha256 "060ef91d1190cae28a219c41a20b6b0bad481baba33c32a3cd91eb682cc2358a"
 
   def install
     bin.install "nepcal"
