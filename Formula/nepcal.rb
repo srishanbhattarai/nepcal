0c67bc1f28872461051d28896170b2daf68ca4df
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-09 11:54:20 +0545

    Brew formula update for nepcal version v0.5.0

--- Formula/nepcal.rb
+++ Formula/nepcal.rb
@@ -1,9 +1,9 @@
 class Nepcal < Formula
   desc "Equivalent of Linux's cal, for Nepali dates"
   homepage "https://github.com/nepcal/nepcal"
-  url "https://github.com/srishanbhattarai/nepcal/releases/download/v0.4.0/nepcal_0.4.0_darwin_amd64.tar.gz"
-  version "0.4.0"
-  sha256 "060ef91d1190cae28a219c41a20b6b0bad481baba33c32a3cd91eb682cc2358a"
+  url "https://github.com/srishanbhattarai/nepcal/releases/download/v0.5.0/nepcal_0.5.0_darwin_amd64.tar.gz"
+  version "0.5.0"
+  sha256 "021e4bede0bf42d771b8cabaa3d55763c2fe180280baabdde2ffd5433b6e0235"
 
   def install
     bin.install "nepcal"
