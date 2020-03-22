67cfd47759f3f55f014524daff9ea064f8fa94d2
Srishan Bhattarai <srishanbhattarai@gmail.com>
2020-03-21 23:41:30 -0500

    Update nepcal CLI to new package

--- cmd/nepcal/cli.go
+++ cmd/nepcal/cli.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
-	"github.com/srishanbhattarai/nepcal/dateconv"
+	"github.com/srishanbhattarai/nepcal/nepcal"
 	"github.com/urfave/cli"
 )
 
@@ -23,7 +25,10 @@ func (nepcalCli) showCalendar(c *cli.Context) {
 // Shows the date for the provided time. Returns a cli 'action'.
 func (nepcalCli) showDate(w io.Writer, t time.Time) func(c *cli.Context) {
 	return func(c *cli.Context) {
-		showDateBS(w, dateconv.ToBS(t), t.Weekday())
+		// This will stop working in 2090 (:
+		bs := nepcal.FromGregorianUnchecked(t)
+
+		fmt.Fprintln(w, bs.String())
 	}
 }
 
@@ -35,15 +40,15 @@ func (nepcalCli) convADToBS(c *cli.Context) {
 	}
 
 	mm, dd, yy, _ := parseRawDate(c.Args().First())
-	adDate := toTime(yy, time.Month(mm), dd)
-	if err := dateconv.CheckBoundsAD(adDate); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+
+	ad := gregorian(yy, mm, dd)
+	bs, err := nepcal.FromGregorian(ad)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Please supply a date after 04/14/1943.")
 		return
 	}
 
-	bsDate := dateconv.ToBS(adDate)
-
-	showDateBS(writer, bsDate, adDate.Weekday())
+	fmt.Fprintln(writer, bs.String())
 }
 
 // Convert BS date to AD date after validation.
@@ -54,16 +59,14 @@ func (nepcalCli) convBSToAD(c *cli.Context) {
 	}
 
 	mm, dd, yy, _ := parseRawDate(c.Args().First())
-	bsdate := dateconv.NewBSDate(yy, mm, dd)
-	if err := bsdate.CheckBounds(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+
+	d, err := nepcal.Date(yy, nepcal.Month(mm), dd)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Please ensure the date is between 1/1/2000 and 12/30/2090")
 		return
 	}
 
-	adyy, admm, addd := dateconv.ToAD(dateconv.NewBSDate(yy, mm, dd)).Date()
-	date := toTime(adyy, time.Month(admm), addd)
-
-	showDateAD(writer, date)
+	printGregorian(writer, d.Gregorian())
 }
 
 // Validates the arguments provided to the program.
@@ -75,4 +78,48 @@ func validateArgs(c *cli.Context) bool {
 	_, _, _, ok := parseRawDate(c.Args().First())
 
 	return ok
+}
+
+// Parse user input raw date into valid dd, mm, yy format. The last parameter is a boolean indicating if
+// the date is valid or not.
+func parseRawDate(rawDate string) (int, int, int, bool) {
+	dateParts := strings.Split(rawDate, "-")
+	if len(dateParts) != 3 {
+		return -1, -1, -1, false
+	}
+
+	mm, err := strconv.Atoi(dateParts[0])
+	if err != nil {
+		return -1, -1, -1, false
+	}
+
+	dd, err := strconv.Atoi(dateParts[1])
+	if err != nil {
+		return -1, -1, -1, false
+	}
+
+	yy, err := strconv.Atoi(dateParts[2])
+	if err != nil {
+		return -1, -1, -1, false
+	}
+
+	if dd < 1 || dd > 31 || mm < 1 || mm > 12 || len(dateParts[2]) != 4 {
+		return -1, -1, -1, false
+	}
+
+	return mm, dd, yy, true
+}
+
+func printGregorian(w io.Writer, t time.Time) {
+	adyy, _, addd := t.Date()
+	month := t.Month()
+	weekday := t.Weekday()
+
+	fmt.Fprintf(w, "%s %d, %d %s\n", month, addd, adyy, weekday)
+}
+
+// gregorian creates a new time.Time with the basic yy/mm/dd parameters.
+// Crucially, the time returned is in UTC.
+func gregorian(yy, mm, dd int) time.Time {
+	return time.Date(yy, time.Month(mm), dd, 0, 0, 0, 0, time.UTC)
 }
--- cmd/nepcal/main.go
+++ cmd/nepcal/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 
-	"github.com/srishanbhattarai/nepcal/dateconv"
 	"github.com/urfave/cli"
 )
 
@@ -68,60 +64,4 @@ func bootstrapCli() *cli.App {
 	}
 
 	return app
-}
-
-// Parse user input raw date into valid dd, mm, yy format. The last parameter is a boolean indicating if
-// the date is valid or not.
-func parseRawDate(rawDate string) (int, int, int, bool) {
-	dateParts := strings.Split(rawDate, "-")
-	if len(dateParts) != 3 {
-		return -1, -1, -1, false
-	}
-
-	mm, err := strconv.Atoi(dateParts[0])
-	if err != nil {
-		return -1, -1, -1, false
-	}
-
-	dd, err := strconv.Atoi(dateParts[1])
-	if err != nil {
-		return -1, -1, -1, false
-	}
-
-	yy, err := strconv.Atoi(dateParts[2])
-	if err != nil {
-		return -1, -1, -1, false
-	}
-
-	if dd < 1 || dd > 31 || mm < 1 || mm > 12 || len(dateParts[2]) != 4 {
-		return -1, -1, -1, false
-	}
-
-	return mm, dd, yy, true
-}
-
-func showDateAD(w io.Writer, t time.Time) {
-	adyy, _, addd := t.Date()
-
-	month := t.Month().String()
-	weekday := t.Weekday()
-
-	fmt.Fprintf(w, "%s %d, %d %s\n", month, addd, adyy, weekday)
-}
-
-// showDate prints the current B.S. date
-func showDateBS(w io.Writer, bs dateconv.BSDate, wd time.Weekday) {
-	bsyy, bsmm, bsdd := bs.Date()
-
-	month, monthOk := dateconv.GetBSMonthName(time.Month(bsmm))
-	weekday, weekdayOk := dateconv.GetNepWeekday(wd)
-
-	if monthOk && weekdayOk {
-		fmt.Fprintf(w, "%s %d, %d %s\n", month, bsdd, bsyy, weekday)
-	}
-}
-
-// toTime creates a new time.Time with the basic yy/mm/dd parameters.
-func toTime(yy int, mm time.Month, dd int) time.Time {
-	return time.Date(yy, mm, dd, 0, 0, 0, 0, time.UTC)
 }
